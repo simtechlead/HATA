4ac9838bacf916d2ba849cd2d5353e5c6f42962b
simtech <150487406+simtechlead@users.noreply.github.com>
2023-11-19 05:37:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,50 +4,59 @@ import time
 from openai import OpenAI
 
 # Set up the page configuration and title
-st.set_page_config(page_title="HATA Chat Interface", layout="wide")
+st.set_page_config(page_title="HATA Chat Interface")
 st.title('HATA Chat Interface')
 
 # Function to interact with OpenAI API
 def interact_with_openai(user_message):
-    # Placeholder for the actual OpenAI API interaction
-    # ...
+    try:
+        openai_key = os.environ['OPENAI_KEY']
+        org_ID = os.environ['ORG_ID']
+
+        client = OpenAI(organization=org_ID, api_key=openai_key)
+        assistant = client.beta.assistants.retrieve("asst_W9WhhX3DRDu8e0A5T5TjQMup")
+        thread = client.beta.threads.create()
+        message = client.beta.threads.messages.create(thread_id=thread.id, role="user", content=user_message)
+        run = client.beta.threads.runs.create(thread_id=thread.id, assistant_id=assistant.id)
+
+        while True:
+            run_status = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+            if run_status.status == 'completed':
+                break
+            else:
+                time.sleep(1)
+
+        messages = client.beta.threads.messages.list(thread_id=thread.id)
+        responses = []
+        for each in messages:
+            if each.role == 'assistant':
+                responses.append(each.content[0].text.value if isinstance(each.content, list) and hasattr(each.content[0], 'text') else str(each.content))
+        return responses
+
+    except Exception as e:
+        st.error(f"Error: {e}")
+        return []
 
 # Initialize session state for conversation history if not already present
 if 'history' not in st.session_state:
     st.session_state.history = [{"role": "assistant", "content": "How may I assist you today?"}]
 
 # Display chat messages
-def display_messages():
-    # Create a container for messages and use st.expander to make it scrollable
-    with st.container():
-        for message in reversed(st.session_state.history):  # Display from the bottom up
-            role = "You" if message["role"] == "user" else "HATA"
-            st.markdown(f"**{role}:**")
-            st.text_area("", value=message["content"], height=3, disabled=True, key=f"{role}_{time.time()}")
-            st.markdown("<hr>", unsafe_allow_html=True)  # Horizontal line
+for message in st.session_state.history:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-# User input form at the bottom
-def chat_input():
-    with st.container():
-        with st.form("chat_form", clear_on_submit=True):
-            user_input = st.text_input("Send a message:", key="input")
-            submit_button = st.form_submit_button(label='Send')
+# Input form for user message
+with st.form("chat_form", clear_on_submit=True):
+    user_input = st.text_input("Send a message:", key="input")
+    submit_button = st.form_submit_button(label='Send')
 
-        if submit_button and user_input:
-            st.session_state.history.append({"role": "user", "content": user_input})
-            responses = interact_with_openai(user_input)
-            for response in responses:
-                st.session_state.history.append({"role": "assistant", "content": response})
-            st.experimental_rerun()
+# Handle message submission
+if submit_button and user_input:
+    st.session_state.history.append({"role": "user", "content": user_input})
+    responses = interact_with_openai(user_input)
+    for response in responses:
+        st.session_state.history.append({"role": "assistant", "content": response})
 
-# Layout the chat history and the input area
-display_messages()
-chat_input()
-
-# To make sure the latest message is visible, we can use an anchor (a placeholder)
-# and scroll to its location after a new message is added
-if 'anchor' not in st.session_state:
-    st.session_state.anchor = st.empty()
-
-st.session_state.anchor.markdown(" ")  # This space will serve as an anchor
-st.script_request_queue.enqueue("scrollTo", {"id": st.session_state.anchor._anchor_id})
+    # Clear the input box by rerunning the app to update the conversation history
+    st.experimental_rerun()
