ea8e6e772280a61f9954a77c303e5ac2b273b518
simtech <150487406+simtechlead@users.noreply.github.com>
2023-11-19 05:35:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,59 +4,75 @@ import time
 from openai import OpenAI
 
 # Set up the page configuration and title
-st.set_page_config(page_title="HATA Chat Interface")
+st.set_page_config(page_title="HATA Chat Interface", layout="wide")
 st.title('HATA Chat Interface')
 
 # Function to interact with OpenAI API
 def interact_with_openai(user_message):
-    try:
-        openai_key = os.environ['OPENAI_KEY']
-        org_ID = os.environ['ORG_ID']
-
-        client = OpenAI(organization=org_ID, api_key=openai_key)
-        assistant = client.beta.assistants.retrieve("asst_W9WhhX3DRDu8e0A5T5TjQMup")
-        thread = client.beta.threads.create()
-        message = client.beta.threads.messages.create(thread_id=thread.id, role="user", content=user_message)
-        run = client.beta.threads.runs.create(thread_id=thread.id, assistant_id=assistant.id)
-
-        while True:
-            run_status = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-            if run_status.status == 'completed':
-                break
-            else:
-                time.sleep(1)
-
-        messages = client.beta.threads.messages.list(thread_id=thread.id)
-        responses = []
-        for each in messages:
-            if each.role == 'assistant':
-                responses.append(each.content[0].text.value if isinstance(each.content, list) and hasattr(each.content[0], 'text') else str(each.content))
-        return responses
-
-    except Exception as e:
-        st.error(f"Error: {e}")
-        return []
+    # Placeholder for API interaction code
+    # Simulate a response for demonstration purposes
+    return ["This is a simulated response."]
 
 # Initialize session state for conversation history if not already present
 if 'history' not in st.session_state:
-    st.session_state.history = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.session_state.history = []
 
-# Display chat messages
-for message in st.session_state.history:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+if 'translation' not in st.session_state:
+    st.session_state.translation = []
 
-# Input form for user message
-with st.form("chat_form", clear_on_submit=True):
-    user_input = st.text_input("Send a message:", key="input")
-    submit_button = st.form_submit_button(label='Send')
+if 'phonetics' not in st.session_state:
+    st.session_state.phonetics = []
 
-# Handle message submission
-if submit_button and user_input:
-    st.session_state.history.append({"role": "user", "content": user_input})
-    responses = interact_with_openai(user_input)
-    for response in responses:
-        st.session_state.history.append({"role": "assistant", "content": response})
+if 'additional_info' not in st.session_state:
+    st.session_state.additional_info = []
 
-    # Clear the input box by rerunning the app to update the conversation history
+# Layout with fixed columns
+col1, col2, col3, col4 = st.columns(4)
+
+# Chat input and history
+with col1:
+    st.subheader("Chat input:")
+    user_input = st.text_input("Enter your message:", key="chat_input")
+    if st.button('Send', key="send_button"):
+        st.session_state.history.append(user_input)  # Add to chat history
+        responses = interact_with_openai(user_input)  # Get the response
+        st.session_state.translation.append(responses[0])  # Add to translation
+        st.session_state.phonetics.append("Phonetic representation")  # Placeholder
+        st.session_state.additional_info.append("Additional information")  # Placeholder
+
+    st.subheader("Chat history:")
+    for message in reversed(st.session_state.history):  # Display from the bottom up
+        st.text(message)
+
+# Translation column
+with col2:
+    st.subheader("Translation:")
+    for translation in st.session_state.translation:
+        st.text(translation)
+
+# Phonetics column
+with col3:
+    st.subheader("Phonetics:")
+    for phonetic in st.session_state.phonetics:
+        st.text(phonetic)
+
+# Additional information column
+with col4:
+    st.subheader("Additional information:")
+    for info in st.session_state.additional_info:
+        st.text(info)
+
+# Clear the input box by rerunning the app to update the conversation history
+if 'rerun' in st.session_state:
+    st.session_state.rerun = False
     st.experimental_rerun()
+
+# Handle message submission at the end to allow for rerun to clear the input box
+if st.session_state.get('rerun', False):
+    user_input = st.session_state.get('chat_input', '')
+    if user_input:
+        st.session_state.history.append({"role": "user", "content": user_input})
+        responses = interact_with_openai(user_input)
+        for response in responses:
+            st.session_state.history.append({"role": "assistant", "content": response})
+        st.session_state['rerun'] = True  # Set the flag to rerun the app
