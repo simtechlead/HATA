411dcc0e40c552d484914890ac766b9eacb43e51
simtech <150487406+simtechlead@users.noreply.github.com>
2023-11-19 03:23:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,46 +13,22 @@ st.title('HATA Chat Interface')
 # Function to interact with OpenAI API
 def interact_with_openai(user_message):
     try:
-        # Retrieve API key and organization ID from Streamlit's settings
         openai_key = os.environ['OPENAI_KEY']
         org_ID = os.environ['ORG_ID']
 
-        client = OpenAI(
-            organization=org_ID,
-            api_key=openai_key
-        )
-
-        # Retrieve a specific assistant
+        client = OpenAI(organization=org_ID, api_key=openai_key)
         assistant = client.beta.assistants.retrieve("asst_W9WhhX3DRDu8e0A5T5TjQMup")
-
-        # Create a new thread
         thread = client.beta.threads.create()
+        message = client.beta.threads.messages.create(thread_id=thread.id, role="user", content=user_message)
+        run = client.beta.threads.runs.create(thread_id=thread.id, assistant_id=assistant.id)
 
-        # Send a message to the thread
-        message = client.beta.threads.messages.create(
-            thread_id=thread.id,
-            role="user",
-            content=user_message
-        )
-
-        # Run the assistant
-        run = client.beta.threads.runs.create(
-            thread_id=thread.id,
-            assistant_id=assistant.id
-        )
-
-        # Wait for completion
         while True:
-            run_status = client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
-                run_id=run.id
-            )
+            run_status = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
             if run_status.status == 'completed':
                 break
             else:
                 time.sleep(1)
 
-        # Retrieve and return responses
         messages = client.beta.threads.messages.list(thread_id=thread.id)
         responses = []
         for each in messages:
@@ -64,12 +40,15 @@ def interact_with_openai(user_message):
         st.error(f"Error: {e}")
         return []
 
-# Display conversation history
+# Display conversation history in a more chat-like format
+st.write("Chat:")
 for index, chat in enumerate(st.session_state.history):
     sender = chat["sender"]
     message = chat["message"]
-    key = f"{sender}_{index}"
-    st.text_area("", value=message, key=key, height=75, disabled=True)
+    if sender == "You":
+        st.text_area(f"You: {message}", key=f"user_{index}", height=50, disabled=True)
+    else:
+        st.text_area(f"HATA: {message}", key=f"hata_{index}", height=50, disabled=True)
 
 # User input form at the bottom
 with st.form(key='user_input_form'):
@@ -78,14 +57,9 @@ with st.form(key='user_input_form'):
 
 # On form submission, append the message and get a response
 if submit_button and user_input:
-    # User's message
     st.session_state.history.append({"sender": "You", "message": user_input})
-
-    # OpenAI's response
     with st.spinner('Talking to HATA...'):
         responses = interact_with_openai(user_input)
         for response in responses:
             st.session_state.history.append({"sender": "HATA", "message": response})
-    
-    # Rerun the app to update the conversation history and clear the input box
     st.experimental_rerun()
