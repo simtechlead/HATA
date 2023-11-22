acb0e1aa55e1b23f4e8a689016e7cd2f4bab0986
simtech <150487406+simtechlead@users.noreply.github.com>
2023-11-22 15:19:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Hata")
 st.title('Penerjemah Indonesia-Simalungun')
 
 # Add user guide
-st.info("""Untuk terjemahan, masukkan kata di kolom chat. Contoh: "pergi"
+st.info("""Untuk terjemahan, masukkan kata di kolom chat. Contoh: "pergi".
 Untuk penjelasan, "Jelaskan lebih detil kata laho"  """)
 
 # Function to interact with OpenAI API
